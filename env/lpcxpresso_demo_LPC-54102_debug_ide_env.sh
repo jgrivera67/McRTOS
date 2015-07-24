bb283eae1b044ee88594b4e26854ea72322a7ddc
J. German Rivera <Jose.G.Rivera@freescale.com>
2015-07-23 19:46:25 -0500

    Fixed my_update_flash for LPC54102 for Linux
    Signed-off-by: J. German Rivera <German.Rivera@freescale.com>

--- env/lpcxpresso_demo_LPC-54102_debug_ide_env.sh
+++ env/lpcxpresso_demo_LPC-54102_debug_ide_env.sh
@@ -9,9 +9,9 @@ function my_update_flash
 {
     typeset dest_dir
 
-    echo "copying $ELF_FILE to $PLATFORM flash ..."
+    echo "copying $BIN_FILE to $PLATFORM flash ..."
 
-    /usr/local/lpcxpresso_7.8.0_426/lpcxpresso/bin/crt_emu_cm_redlink -flash-load-exec "$BIN_FILE" -g -2 -vendor=NXP -pLPC54102J512 #-flash-driver=LPC5410x_512K.cfx
+    /usr/local/lpcxpresso_7.8.0_426/lpcxpresso/bin/crt_emu_cm_redlink -flash-load-exec "$BIN_FILE" -g -2 -vendor=NXP -pLPC54102J512 -flash-driver=LPC5410x_512K.cfx
     status=$?
     if [ $status != 0 ]; then
 	echo "*** crt_emu_cm_redlink failed with error $status ***"
